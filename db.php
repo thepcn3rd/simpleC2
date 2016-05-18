0fba29150d94352db4257004aeb811e0cae13401
thepcn3rd <ltrappett@gmail.com>
2016-05-18 16:03:25 -0600

    Modified the db.php file

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-	$mysqli = new mysqli("localhost","wordpress","supersecurepassword","command");
+	$mysqli = new mysqli("localhost","wordpress","mysecurepassword","command");
 	if ($mysqli->connect_error) {
 		die("Connect error $mysqli->connect_error");
 	}
